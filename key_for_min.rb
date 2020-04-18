dad23da3a279b2363e0b1d558720d4ae9c3af02e
David Munro <dbhmunro@gmail.com>
2020-04-18 02:59:00 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   min_key = nil
-  min_value = Float::INFINITY
+  min_value = Float::INFINITY # float is a class for inexact real numbers, i.e. infinity
   name_hash.each do |name, value|
     if (value < min_value)
       min_value = value
